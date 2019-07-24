a1de0af2897975b02f59a9a8a02a90fa14e9f902
NikkiKauffmann <50151179+NikkiKauffmann@users.noreply.github.com>
2019-07-24 19:38:46 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# portfolio
+My website is a portfolio/gallery website. I have experimented with several javascript and css coding styles and have finally narrowed down which styles look best for my website
+I have had a few bumps in the road in terms of tracking down errors in my coding, but I have found what I needed to fix and while it is not perfect, it was a fantastic learning experience.
---
+++ app.js
@@ -0,0 +1,19 @@
+function togglemenu () {
+    var index = document.getElementsByClassName ('link')[0]
+    $('.link').toggle();
+    document.getElementById("togglemenu").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
